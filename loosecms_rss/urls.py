1f88fdd7e4da4fd2a458622c43e444dce7a01dd1
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-09-30 17:39:00 +0300

    Update url definitions according to new core.

--- loosecms_rss/urls.py
+++ loosecms_rss/urls.py
@@ -2,8 +2,6 @@
 from django.conf.urls import url
 from .views import *
 
-app_urlpatterns = [
+urlpatterns = [
     url(r'^rss/(?P<rss_slug>[0-9A-Za-z-_.]+)/$', rss, name='rss-info'),
 ]
-
-urlpatterns = []
